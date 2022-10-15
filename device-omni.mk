57fc236c9c6779ffb2f759ebd484e9fb8b0c17ba
flawedworld <flawedworld@flawed.world>
2022-10-15 19:25:55 +0100

    pantah: Add sysconfig and permissions from stock OS
    Co-Authored-By: Vladislav <stvlad06@gmail.com>
    Change-Id: Icb979e9fc83712c788bd95363ecb1467b733fed5

--- device-omni.mk
+++ device-omni.mk
@@ -1,24 +1,7 @@
 # Overlays
-<<<<<<< Updated upstream
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-omni
-=======
 DEVICE_PACKAGE_OVERLAYS += device/google/pantah/overlay-omni
 
 # sysconfig and permissions XML from stock
 PRODUCT_COPY_FILES += \
 	device/google/pantah/conf/product-sysconfig-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/product-sysconfig-stock.xml \
     device/google/pantah/permissions/product-permissions-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product-permissions-stock.xml
-
-# EUICC
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
-    device/google/pantah/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml \
-    device/google/pantah/permissions/permissions_com.google.euiccpixel.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.google.euiccpixel.xml
-
-PRODUCT_PACKAGES += \
-    EuiccSupportPixelOverlay
-
-# wireless_charger HAL service
-include device/google/gs-common/wireless_charger/wireless_charger.mk
-
->>>>>>> Stashed changes
